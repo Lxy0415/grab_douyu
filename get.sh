ca501d9a5ee812a7f808d718ad67bec06858066d
lxy0415 <li-cannon@163.com>
2017-08-14 16:33:35 +0800

    update

--- get.sh
+++ get.sh
@@ -15,8 +15,17 @@ echo change the url to
 flv_url2=`echo $flv_url | sed -e "s/_[0..9]\{3\}.flv/.flv/g"`
 echo $flv_url2
 
+i=0
 while true 
 do
-    output=`date "+%s"`.flv
+    #output=`date "+%s"`.flv
+    output=`date "+%F@%T"`.flv
     wget -O $output $flv2_url
+    let i=i+1
+    if [ `wc -c < $output` == 0 ]; then
+        rm $output
+    else
+        echo $output start after $i trys
+        echo $output stop at `date "+%F %T"`
+    fi
 done
